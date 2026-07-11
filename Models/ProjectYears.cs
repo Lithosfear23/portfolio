b62daccffb579b05d03dc972d6883659f2f978df
Lithosfear23 <renz.malabanan1816@gmail.com>
2026-07-12 01:53:33 +0800

    Completed side quest section design

---
+++ Models/ProjectYears.cs
@@ -0,0 +1,8 @@
+﻿namespace Malabanan_Portfolio.Models
+{
+    public class ProjectYears
+    {
+        public int Year { get; set; }
+        public bool Selected { get; set; } = false;
+    }
+}
---
+++ Models/ProjectsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Malabanan_Portfolio.Models
+{
+    public class ProjectsModel
+    {
+        public DateTime Date { get; set; }
+        public string ProjectName { get; set; } = string.Empty;
+        public string ProjectStatus { get; set; } = string.Empty;
+        public bool SelectedProject { get; set; } = false;
+    }
+}
